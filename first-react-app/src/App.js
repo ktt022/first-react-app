b255658d4f91fa9c57df30f5d0574989b1443a52
Kayla Tran <ktt022@ucsd.edu>
2018-12-12 12:39:27 -0800

    test

---
+++ first-react-app/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit the file <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+class project-box extends Component {
+  consructor(props){
+    super(props);
+    
+  }
+  render() {
+    
+  }
+}
+
+export default App;
